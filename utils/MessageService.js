635ef44236c3caa73512321910981aed0530dde0
Thang Le Quoc <silverhair.guy@gmail.com>
2017-09-02 17:58:30 +0700

    get general message, specific message service

--- utils/MessageService.js
+++ utils/MessageService.js
@@ -2,11 +2,37 @@
  * MessageService is use to resolve AQI Message
  */
 module.exports = {
-  getGeneralMessage(pollutantCode, index, generalMessages) {
-    
+  getGeneralMessage(index, generalMessages) {
+    let result = {
+      category: undefined,
+      message: undefined
+    }
+    generalMessages.forEach((level) => {
+      if (index >= level.index.min && index <= level.index.max) {
+        result.category = level.category;
+        result.message = level.guidance;
+      }
+    });
+    return result;
   },
 
   getSpecificMessage(pollutantCode, index, specificMessages) {
+    let result = {
+      healthEffectsStatements: undefined,
+      guidanceStatement: undefined
+    }
 
+    let specificMessage;
+    specificMessages.forEach((messageByPollutant) => {
+      if (messageByPollutant.code == pollutantCode)
+        specificMessage = messageByPollutant;
+    });
+    specificMessage.forEach((message) => {
+      if(index >= message.aqiLevel.index.min && index <= message.aqiLevel.index.max){
+        result.healthEffectsStatements = message.aqiLevel.healthEffectsStatements;
+        result.guidanceStatement = message.aqiLevel.guidance;
+      }
+    });
+    return result;
   }
 }
